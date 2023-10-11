c5cf4ac3ff1283a67164aa83f6c43b6c54c48953
Merlin04 <Merlin04@users.noreply.github.com>
2023-10-11 00:16:20 -0700

    add version

--- bin/client.ml
+++ bin/client.ml
@@ -1,8 +1,9 @@
 open Grpc_lwt
 open Lwt.Syntax
+open Ocaml_protoc_plugin
 open Arduino_cli.Commands.Cc.Arduino.Cli.Commands.V1
 
-let call_server address port req =
+let client address port : H2_lwt_unix.Client.t Lwt.t =
   (* Setup Http/2 connection *)
   let* addresses =
     Lwt_unix.getaddrinfo address (string_of_int port)
@@ -11,25 +12,50 @@ let call_server address port req =
   let socket = Lwt_unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in
   let* () = Lwt_unix.connect socket (List.hd addresses).Unix.ai_addr in
   let error_handler _ = print_endline "error" in
-  let* connection =
-    H2_lwt_unix.Client.create_connection ~error_handler socket
-  in
+  H2_lwt_unix.Client.create_connection ~error_handler socket
 
-  (* codegen *)
-  let open Ocaml_protoc_plugin in
+let core_call conn rpc handler =
+  Client.call ~service:"cc.arduino.cli.commands.v1.ArduinoCoreService" ~rpc:rpc
+    ~do_request:(H2_lwt_unix.Client.request conn ~error_handler:ignore)
+    ~handler:handler
+    ()
+
+let call_create conn req =
   let encode, decode = Service.make_client_functions ArduinoCoreService.create in
   let enc = encode req |> Writer.contents in
+  core_call conn "Create" (Client.Rpc.unary enc ~f:(fun decoder ->
+    let+ decoder = decoder in
+    match decoder with
+      | Some decoder -> (
+        Reader.create decoder |> decode |> function
+          | Ok v -> v
+          | Error e ->
+            failwith (Printf.sprintf "Could not decode request: %s" (Result.show_error e)))
+      | None -> failwith "Failed to create instance"))
 
-  Client.call ~service:"cc.arduino.cli.commands.v1.ArduinoCoreService" ~rpc:"Create"
-    ~do_request:(H2_lwt_unix.Client.request connection ~error_handler:ignore)
-    ~handler:
-      (Client.Rpc.unary enc ~f:(fun decoder ->
-        let+ decoder = decoder in
-        match decoder with
-          | Some decoder -> (
-            Reader.create decoder |> decode |> function
-              | Ok v -> v
-              | Error e ->
-                failwith (Printf.sprintf "Could not decode request: %s" (Result.show_error e)))
-          | None -> ArduinoCoreService.Create.Response.make ()))
-  ()
+let call_init conn req =
+  let encode, decode = Service.make_client_functions ArduinoCoreService.init in
+  let enc = encode req |> Writer.contents in
+  let* _stream = core_call conn "Init" (Client.Rpc.server_streaming enc ~f:(fun responses ->
+    let stream = Lwt_stream.map (fun str ->
+      Reader.create str |> decode |> function
+        | Ok v -> print_endline (InitResponse.show v); v
+        | Error e -> failwith (Printf.sprintf "Could not decode request: %s" (Result.show_error e))
+    ) responses in Lwt_stream.to_list stream
+  ))
+  in
+  Lwt.return_unit
+
+let call_version conn =
+  let encode, decode = Service.make_client_functions ArduinoCoreService.version in
+  let enc = encode (VersionRequest.make ()) |> Writer.contents in
+  core_call conn "Version" (Client.Rpc.unary enc ~f:(fun decoder ->
+    let+ decoder = decoder in
+    match decoder with
+      | Some decoder -> (
+        Reader.create decoder |> decode |> function
+          | Ok v -> v
+          | Error e ->
+            failwith (Printf.sprintf "Could not decode request: %s" (Result.show_error e)))
+      | None -> failwith "Failed to get version"))
+
--- bin/main.ml
+++ bin/main.ml
@@ -1,13 +1,16 @@
 open Client
 open Arduino_cli.Commands.Cc.Arduino.Cli.Commands.V1
+open Lwt.Infix
+open Lwt.Syntax
 
 let () =
-  let open Lwt.Syntax in
   let port = 50051 in
   let address = "localhost" in
   let req = CreateRequest.make () in
-  Lwt_main.run
-    (let+ res = call_server address port req in
-      match res with
-        | Ok (res, _) -> print_endline (CreateResponse.show res)
-        | Error _ -> print_endline "Error")
+  Lwt_main.run (
+    let* connection = client address port in
+    let* instance = call_create connection req >|= Result.get_ok >|= fst >|= Option.get in
+    let* () = call_init connection (InitRequest.make ~instance ()) in
+    let* () = call_version connection >|= Result.get_ok >|= fst >>= Lwt_io.printl in
+    Lwt.return_unit
+  )
