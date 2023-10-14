9cdeb284173d891391a44a78916b59aca522ff56
Merlin04 <Merlin04@users.noreply.github.com>
2023-10-14 11:42:00 -0700

    Serial monitor recv

--- bin/client.ml
+++ bin/client.ml
@@ -24,26 +24,41 @@ let core_call conn rpc handler =
 type ('req, 'rep) reqrep = (module Ocaml_protoc_plugin.Service.Message with type t = 'req) *
                         (module Ocaml_protoc_plugin.Service.Message with type t = 'rep)
 
+let unwrap_decode_res = function
+  | Ok v -> v
+  | Error e -> failwith (Printf.sprintf "Could not decode response: %s" (Result.show_error e))
+
 let call_unary (type t_req) (type t_rep) (reqrep : (t_req, t_rep) reqrep) rpc ~conn ~req =
   let encode, decode = Service.make_client_functions reqrep in
   let enc = encode req |> Writer.contents in
   core_call conn rpc (Client.Rpc.unary enc ~f:(fun decoder ->
-    decoder >|= Option.get >|= Reader.create >|= decode >|= function
-      | Ok v -> v
-      | Error e -> failwith (Printf.sprintf "Could not decode response: %s" (Result.show_error e))
+    decoder >|= Option.get >|= Reader.create >|= decode >|= unwrap_decode_res
   ))
 
-let call_stream (type t_req) (type t_rep) (reqrep : (t_req, t_rep) reqrep) rpc ~conn ~req =
+let decode_stream (decode : Reader.t -> 'a Result.t) s : 'a Lwt_stream.t =
+  Lwt_stream.map (fun r -> r |> Reader.create |> decode |> unwrap_decode_res) s
+
+(* blocking - promise doesn't resolve until network request closes *)
+let call_server_stream (type t_req) (type t_rep) (reqrep : (t_req, t_rep) reqrep) rpc ~conn ~req =
   let encode, decode = Service.make_client_functions reqrep in
   let enc = encode req |> Writer.contents in
   core_call conn rpc (Client.Rpc.server_streaming enc ~f:(fun responses ->
-    Lwt_stream.map (fun r -> Reader.create r |> decode |> function
-      | Ok v -> v
-      | Error e -> failwith (Printf.sprintf "Could not decode response: %s" (Result.show_error e))
-    ) responses |> Lwt.return
+    responses |> decode_stream decode |> Lwt.return
   ))
 
+(* non-blocking - returns send/responses in addition to promise that resolves when request closes *)
+let call_bidi_stream_nb (type t_req) (type t_rep) (reqrep : (t_req, t_rep) reqrep) rpc ~conn =
+  let encode, decode = Service.make_client_functions reqrep in
+  let (p, res) = Lwt.wait () in
+  let call_p = core_call conn rpc (Client.Rpc.bidirectional_streaming ~f:(fun send responses ->
+    ((fun (v : t_req Option.t) -> v |> Option.map (fun r -> encode r |> Writer.contents) |> send),
+      decode_stream decode responses) |> Lwt.wakeup_later res; Lwt.return_unit
+  )) in
+  let* (send, responses) = p in
+  (send, responses, call_p) |> Lwt.return
+
 let call_create = call_unary ArduinoCoreService.create "Create" ~req:(CreateRequest.make ())
-let call_init = call_stream ArduinoCoreService.init "Init"
+let call_init = call_server_stream ArduinoCoreService.init "Init"
 let call_version = call_unary ArduinoCoreService.version "Version" ~req:(VersionRequest.make ())
 let call_board_list = call_unary ArduinoCoreService.boardList "BoardList"
+let call_monitor = call_bidi_stream_nb ArduinoCoreService.monitor "Monitor"
--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,7 @@
 open Client
 open Arduino_cli.Commands.Cc.Arduino.Cli.Commands.V1
 open Arduino_cli.Board.Cc.Arduino.Cli.Commands.V1
+open Arduino_cli.Monitor.Cc.Arduino.Cli.Commands.V1
 open Lwt.Infix
 open Lwt.Syntax
 
@@ -26,10 +27,26 @@ let () =
       | [] -> Lwt.return_unit
       | r -> r |> Lwt_list.iter_s (fun r -> r |> InitResponse.show |> Lwt_io.printl) >>= failwith "Unexpected output from init" in
     let* () = call_version ~conn >|= Result.get_ok >|= fst >>= Lwt_io.printl in
-    let* () = call_board_list ~conn ~req:(BoardListRequest.make ~instance ())
+    let* port = call_board_list ~conn ~req:(BoardListRequest.make ~instance ())
       >|= Result.get_ok >|= fst
-      >|= BoardListResponse.show
-      >>= Lwt_io.printl in
+      >|= fun ports -> (List.find_map (fun (b : DetectedPort.t) -> match b.matching_boards with
+        | _ :: _ -> b.port
+        | _ -> None) ports
+      |> function
+        | Some p -> p
+        | None -> failwith "Could not find a matching board.") in
+    let* m_call = call_monitor ~conn in
+    let (send, responses, p) = m_call in
+    let p_handler = p >|= Result.get_ok >|= fst in
+    let iter_promise = responses |> Lwt_stream.iter_s (fun (m : MonitorResponse.t) ->
+      Lwt_io.print (match m with
+         | { error = e; _ } when e <> "" -> "--- ERROR: " ^ e ^ "\n"
+         | { success = true; _ } -> "Port successfully opened\n"
+         | { rx_data = d; _ } -> Bytes.to_string d)
+    ) in
+    let () = send (Some (MonitorRequest.make ~instance ~port ())) in
+    let* () = Lwt_io.printlf "Connecting to %s (%s)" (List.assoc_opt "name" port.properties |> Option.value ~default:port.label) port.address in
+    let* () = Lwt.join [p_handler; iter_promise] in
 
     daemon#kill Sys.sigterm;
     Lwt.return_unit
